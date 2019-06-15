16aa100cb44d50d60a723388ba06c4bbc9dfd9de
erkanyildiz <ey@count.ly>
2019-06-15 21:20:58 +0900

    Updated watchOS sample app for WCSession delegate

--- watchos/CountlyTestApp-watchOS WatchKit Extension/ExtensionDelegate.m
+++ watchos/CountlyTestApp-watchOS WatchKit Extension/ExtensionDelegate.m
@@ -6,11 +6,20 @@
 
 #import "ExtensionDelegate.h"
 #import "Countly.h"
+#import "WatchConnectivity/WatchConnectivity.h"
+
+
+@interface ExtensionDelegate () <WCSessionDelegate>
+@end
+
 
 @implementation ExtensionDelegate
 
 - (void)applicationDidFinishLaunching
 {
+    //NOTE: set your WCSession delegate before starting Countly, Countly will override and forward to it
+    WCSession.defaultSession.delegate = self;
+
     CountlyConfig* config = CountlyConfig.new;
     config.appKey = @"YOUR_APP_KEY";
     config.host = @"https://YOUR_COUNTLY_SERVER";
@@ -29,6 +38,15 @@
 - (void)applicationWillResignActive
 {
     [Countly.sharedInstance suspend];
+}
+
+
+#pragma mark - WCSessionDelegate
+
+
+- (void)session:(WCSession *)session didReceiveUserInfo:(NSDictionary<NSString *, id> *)userInfo
+{
+    NSLog(@"didReceiveUserInfo (Original Delegate): %@", userInfo);
 }
 
 @end
